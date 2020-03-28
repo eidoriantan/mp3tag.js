6c3e6009439a2a1f3460e9772f265401de9823c5
Adriane Justine Tan <eidoriantan@gmail.com>
2020-03-28 16:50:42 +0800

    style: Change variable name

--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ const assert = require('assert')
 
 describe('mp3tag.js Usage', function () {
   it('Throws if not an audio file', function () {
-    const textBuffer = new Uint8Array([1, 2, 3, 4, 5])
+    const uint8 = new Uint8Array([1, 2, 3, 4, 5])
     assert.throws(function () {
-      const mp3tag = new MP3Tag(textBuffer.buffer)
+      const mp3tag = new MP3Tag(uint8.buffer)
       return mp3tag.read()
     }, /This format is not yet supported/)
   })
