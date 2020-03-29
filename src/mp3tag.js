107654e45c8f30d1f704dce8884192c069b51dec
Adriane Justine Tan <eidoriantan@gmail.com>
2020-03-29 11:47:40 +0800

    feat: Support Buffer instance

--- src/mp3tag.js
+++ src/mp3tag.js
@@ -8,8 +8,8 @@ export default class MP3Tag {
   set frames (value) { this.tagger.frames = value }
 
   constructor (buffer, options) {
-    if (buffer instanceof ArrayBuffer === false) {
-      throw new TypeError('buffer is not an instance of ArrayBuffer')
+    if (buffer instanceof ArrayBuffer === false || buffer instanceof Buffer) {
+      throw new TypeError('buffer is not an instance of ArrayBuffer or Buffer')
     }
 
     this.name = 'MP3Tag'
