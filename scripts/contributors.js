6b6266b4afa8d419eb4372bc47a6b286b753042b
Adriane Justine Tan <eidoriantan@gmail.com>
2020-04-10 02:14:54 +0800

    perf: Add accepts header

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -3,20 +3,23 @@ const GITHUB_REPO = 'https://api.github.com/repos/eidoriantan/mp3tag.js'
 
 $(document).ready(function () {
   const template = $('#contributor-template').prop('content')
-  $.get(GITHUB_REPO + '/contributors', function (users) {
-    for (let i = 0; i < users.length; i++) {
-      const item = $(template).clone()
-      const user = users[i]
+  $.ajax(GITHUB_REPO + '/contributors', {
+    method: 'GET',
+    accepts: 'application/vnd.github.v3+json',
+    dataType: 'json',
+    success: function (users) {
+      users.forEach(function (user) {
+        const item = $(template).clone()
+        $(item).find('[data-temp=\'user-link\']').attr('href', user.html_url)
+        $(item).find('[data-temp=\'user-image\']').attr({
+          src: user.avatar_url,
+          title: user.login,
+          alt: user.login
+        })
+        $(item).find('[data-temp]').removeAttr('data-temp')
 
-      $(item).find('[data-temp=\'user-link\']').attr('href', user.html_url)
-      $(item).find('[data-temp=\'user-image\']').attr({
-        src: user.avatar_url,
-        title: user.login,
-        alt: user.login
+        $('#contributors').append(item)
       })
-      $(item).find('[data-temp]').removeAttr('data-temp')
-
-      $('#contributors').append(item)
     }
   })
 })
