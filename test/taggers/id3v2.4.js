4c4b8687844e3331a7cd06680541b2f434b39823
Adriane Justine Tan <eidoriantan@gmail.com>
2020-04-13 18:40:02 +0800

    test: Remove deprecated

--- test/taggers/id3v2.4.js
+++ test/taggers/id3v2.4.js
@@ -13,17 +13,20 @@ describe('ID3v2.4', function () {
     ])
 
     this.mp3tag = new MP3Tag(uint8.buffer, { padding: 0 })
-    this.mp3tag.read()
+    this.tagger = this.mp3tag.read()
   })
 
   it('Read data', function () {
-    assert.deepStrictEqual(this.mp3tag.tagger.major, 4)
-    assert.deepStrictEqual(this.mp3tag.frames[0].id, 'TALB')
-    assert.deepStrictEqual(this.mp3tag.frames[0].value, 'ALBUM')
+    assert.deepStrictEqual(this.tagger.major, 4)
+    assert.deepStrictEqual(this.tagger.frames[0].id, 'TALB')
+    assert.deepStrictEqual(this.tagger.frames[0].value, 'ALBUM')
+
+    const frames = this.tagger.getFrames()
+    assert.deepStrictEqual(frames.TALB, ['ALBUM'])
   })
 
   it('Write data', function () {
-    this.mp3tag.frames.push({ id: 'TPE1', value: ['ARTIST1'] })
+    this.tagger.addFrame('TPE1', ['ARTIST1'])
     this.mp3tag.save()
 
     const actual = new Uint8Array(this.mp3tag.buffer)
