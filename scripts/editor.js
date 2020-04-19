61868f24c2be219a7554e9c276dfdb67f1b24a86
Adriane Justine Tan <eidoriantan@gmail.com>
2020-04-19 23:51:36 +0800

    feat: Use MP3Tag.genre

--- scripts/editor.js
+++ scripts/editor.js
@@ -17,42 +17,6 @@ const timeRegex = new RegExp(
   `^(${year}(-${month}(-${day}(T${hour}(:${minute}(:${second})?)?)?)?)?)$`
 )
 
-const GENRES = [
-  "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge",
-  "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "R&B", "Rap",
-  "Reggae", "Rock", "Techno", "Industrial", "Alternative", "Ska", "Death Metal",
-  "Pranks", "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal",
-  "Jazz+Funk", "Fusion", "Trance", "Classical", "Instrumental", "Acid", "House",
-  "Game", "Sound Clip", "Gospel", "Noise", "Alt. Rock", "Bass", "Soul", "Punk",
-  "Space", "Meditative", "Instrumental Pop", "Instrumental Rock", "Ethnic",
-  "Gothic", "Darkwave", "Techno-Industrial", "Electronic", "Pop-Folk",
-  "Eurodance", "Dream", "Southern Rock", "Comedy", "Cult", "Gangsta Rap",
-  "Top 40", "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret",
-  "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal",
-  "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll",
-  "Hard Rock", "Folk", "Folk-Rock", "National Folk", "Swing", "Fast-Fusion",
-  "Bebop", "Latin", "Revival", "Celtic", "Bluegrass", "Avantgarde",
-  "Gothic Rock", "Progressive Rock", "Psychedelic Rock", "Symphonic Rock",
-  "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour",
-  "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony",
-  "Booty Bass", "Primus", "Porn Groove", "Satire", "Slow Jam", "Club",
-  "Tango", "Samba", "Folklore", "Ballad", "Power Ballad", "Rhythmic Soul",
-  "Freestyle", "Duet", "Punk Rock", "Drum Solo", "A Cappella", "Euro-House",
-  "Dance Hall", "Goa", "Drum & Bass", "Club-House", "Hardcore", "Terror",
-  "Indie", "BritPop", "Afro-Punk", "Polsk Punk", "Beat",
-  "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover",
-  "Contemporary Christian", "Christian Rock", "Merengue", "Salsa",
-  "Thrash Metal", "Anime", "JPop", "Synthpop", "Abstract", "Art Rock",
-  "Baroque", "Bhangra", "Big Beat", "Breakbeat", "Chillout", "Downtempo", "Dub",
-  "EBM", "Eclectic", "Electro", "Electroclash", "Emo", "Experimental", "Garage",
-  "Global", "IDM", "Illbient", "Industro-Goth", "Jam Band", "Krautrock",
-  "Leftfield", "Lounge", "Math Rock", "New Romantic", "Nu-Breakz", "Post-Punk",
-  "Post-Rock", "Psytrance", "Shoegaze", "Space Rock", "Trop Rock",
-  "World Music", "Neoclassical", "Audiobook", "Audio Theatre",
-  "Neue Deutsche Welle", "Podcast", "Indie Rock", "G-Funk", "Dubstep",
-  "Garage Rock", "Psybient"
-]
-
 $(document).ready(function () {
   $('#list-wrapper').on('dragenter', function (event) {
     event.preventDefault()
@@ -186,7 +150,7 @@ function displayDetails (tagger) {
     $('#year').val(tagger.year)
     $('#comment').val(tagger.comment)
     if (tagger.track) $('#track').val(tagger.track)
-    $('#genre').val(GENRES[tagger.genre])
+    $('#genre').val(MP3Tag.genre[tagger.genre])
   } else if (tagger.name === 'ID3v2') {
     const frames = tagger.getFrames()
     for (const id in frames) {
@@ -299,7 +263,7 @@ async function writeDetails (tagger, data) {
     tagger.year = $('#year').val()
     tagger.comment = ''
     tagger.track = $('#track').val()
-    tagger.genre = GENRES.indexOf($('#genre').val())
+    tagger.genre = MP3Tag.genre.indexOf($('#genre').val())
   } else if (tagger.name === 'ID3v2') {
     const cover = $('#cover').prop('files')
     const date = {}
