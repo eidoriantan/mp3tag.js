3ebabe205caa6d664a3df2daf53da27b126d1140
Adriane Justine Tan <eidoriantan@gmail.com>
2020-04-27 14:19:00 +0800

    perf: Improve toast

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,9 +1,9 @@
 
-const TOAST_NONE = ['', '']
-const TOAST_SUCCESS = ['check', 'text-white bg-success']
-const TOAST_INFO = ['info-circle', 'text-white bg-info']
-const TOAST_DANGER = ['exclamation-circle', 'text-white bg-danger']
-const TOAST_WARNING = ['exclamation-triangle', 'text-white bg-warning']
+const TOAST_NONE = ''
+const TOAST_SUCCESS = 'text-white bg-success'
+const TOAST_INFO = 'text-white bg-info'
+const TOAST_DANGER = 'text-white bg-danger'
+const TOAST_WARNING = 'text-white bg-warning'
 let toastCounter = 0
 
 function toast (title, message, type = TOAST_NONE) {
@@ -12,9 +12,7 @@ function toast (title, message, type = TOAST_NONE) {
   const id = toastCounter++
 
   $(toast).find('.toast').attr('id', `toast-${id}`)
-  $(toast).find('.toast').addClass('pointer')
-  $(toast).find('.toast *').addClass(type[1])
-  $(toast).find('[data-temp=\'icon\']').addClass(`fa-${type[0]}`)
+  $(toast).find('.toast *').addClass(type)
   $(toast).find('[data-temp=\'title\']').text(title)
   $(toast).find('[data-temp=\'body\']').text(message)
   $(toast).find('[data-temp]').removeAttr('data-temp')
