d1182cc199cb789f3039dd2b5078dc08c85100ea
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-24 02:09:57 +0800

    revert: Remove advanced settings

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -7,16 +7,6 @@ let imageBytes = null
 let imageType = ''
 let mp3tag = null
 
-const year = '(\\d{4})'
-const month = '(0[1-9]|1[0-2])'
-const day = '(0[1-9]|1\\d|2\\d|3[0-1])'
-const hour = '(0\\d|1\\d|2[0-3])'
-const minute = '(0\\d|1\\d|2\\d|3\\d|4\\d|5\\d)'
-const second = minute
-const timeRegex = new RegExp(
-  `^(${year}(-${month}(-${day}(T${hour}(:${minute}(:${second})?)?)?)?)?)$`
-)
-
 $(document).ready(function () {
   blankImage = $('#cover-preview').attr('src')
   $('#list-wrapper').on('dragenter', function (event) {
@@ -53,30 +43,6 @@ $(document).ready(function () {
     imageBytes = new Uint8Array(buffer)
     const url = imageURL(imageBytes, file.type)
     $('#cover-preview').attr('src', url)
-  })
-
-  $('#month').on('change', function () {
-    $('#day').find('option').attr('disabled', null)
-    const month = $(this).val()
-    const year = parseInt($('#year').val())
-    const removeDays = function (...days) {
-      $.each(days, function (index, day) {
-        $('#day').find(`option[value='${day}']`).attr('disabled', true)
-      })
-    }
-
-    switch (month) {
-      case '04': case '06': case '09': case '11':
-        removeDays(31)
-        break
-
-      case '02':
-        removeDays(30, 31)
-        if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-          removeDays(29)
-        }
-        break
-    }
   })
 
   $('#edit-form').submit(function (event) {
@@ -157,20 +123,7 @@ function displayDetails () {
     $('#cover-preview').attr({ src: imageURL(imageBytes, imageType) })
   }
 
-  if (tags.TLAN) $('#language').val(tags.TLAN)
   if (tags.TRCK) $('#track').val(tags.TRCK)
-  if (tags.TSRC) $('#isrc').val(tags.TSRC)
-  if (tags.TDAT) {
-    $('#month').val(tags.TDAT.substr(2, 4))
-    $('#day').val(tags.TDAT.substr(0, 2))
-  }
-  if (tags.TYER) $('#year').val(tags.TYER)
-  if (tags.TDRC) {
-    const result = timeRegex.exec(tags.TDRC)
-    if (result[2]) $('#year').val(result[2])
-    if (result[4]) $('#month').val(result[4])
-    if (result[6]) $('#day').val(result[6])
-  }
   if (tags.TCOM) $('#composer').val(tags.TCOM)
   if (tags.USLT) {
     $('#lyrics').val(
@@ -207,11 +160,8 @@ async function writeDetails () {
   mp3tag.tags.artist = $('#artist').val()
   mp3tag.tags.album = $('#album').val()
   mp3tag.tags.year = $('#year').val()
-  mp3tag.tags.TDAT = $('#day').val() + $('#month').val()
   mp3tag.tags.TRCK = $('#track').val()
   mp3tag.tags.genre = $('#genre').val()
-  mp3tag.tags.TSRC = $('#isrc').val()
-  mp3tag.tags.TLAN = $('#language').val()
   mp3tag.tags.TCOM = $('#composer').val()
 
   if (imageBytes !== null) {
