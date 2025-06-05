99c05ab2f2dc7fc6866b568da357899928506671
Adriane Justine Tan <eidoriantan@icloud.com>
2025-06-05 19:06:41 +0800

    chore: up version

--- types/index.d.ts
+++ types/index.d.ts
@@ -48,7 +48,7 @@ export type MP3TagWriteOptions = RecursivePartial<MP3TagDefaultWriteOptions>;
 
 export class MP3Tag {
   readonly name = 'MP3Tag';
-  readonly version = '3.13.0';
+  readonly version = '3.14.0';
 
   verbose: boolean;
   buffer: MP3Buffer;
